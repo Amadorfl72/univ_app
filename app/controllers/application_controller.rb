628dd8163cb1aa820bf383b9dfe99cad7afb61df
Amador Fernández <amador.fernandez@factorial.co>
2023-01-16 15:49:09 +0100

    Assets fine tuning and Many to Many association

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :require_user
+
+  helper_method :current_user, :logged_in?
+  def current_user
+    # ||= only executes the query if @current_user is not already set
+    @current_user ||= Student.find(session[:student_id]) if session[:student_id]
+  end
+
+  def logged_in?
+      # when using !!, will convert the call to the method response as true/false
+      !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:notice]="You must be logged to perform this action"
+      redirect_to login_path
+    end
+  end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,9 @@
 class StudentsController < ApplicationController
+
+skip_before_action :require_user, only: [:new, :create]
+
 before_action :set_student, only: [:show,:edit,:update]
+before_action :require_same_student, only: [:edit,:update]
   def index
     @students= Student.all
   end
@@ -41,14 +45,19 @@ before_action :set_student, only: [:show,:edit,:update]
   private
 
   def student_params
-    params.require(:student).permit(:name,:email)
+    params.require(:student).permit(:name,:email,:password,:password_confirmation)
   end
 
   def set_student
     @student = Student.find(params[:id])
   end
 
-
+  def require_same_student
+    if current_user!= @student
+      flash[:notice]="You can only edit your own profile"
+      redirect_to student_path(current_user)
+    end
+  end
  
 
 end
