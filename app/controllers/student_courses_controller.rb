e37e828102b2d22cbdda65d94238d6f3336ff1a9
Amador Fernández <amador.fernandez@factorial.co>
2023-01-17 10:12:15 +0100

    Course final end. From here beyond is on you

---
+++ app/controllers/student_courses_controller.rb
@@ -0,0 +1,17 @@
+class StudentCoursesController < ApplicationController
+
+  def create
+    course_to_add=Course.find(params[:course_id])
+    unless current_user.courses.include?(course_to_add[0])
+      StudentCourse.create(course: course_to_add,student: current_user)
+      flash[:notice]="You've been succesfully enrolled in #{course_to_add.name}"
+      redirect_to current_user
+    else
+      redirect_to root_path
+      flash[:notice]="You are already enrolled on this course "
+    end
+
+  end
+
+
+end
